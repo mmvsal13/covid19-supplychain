9d63593eb7c72f81662f8c9c5dada8be8104f7b9
Kentaro Vadney <kentarovadney@berkeley.edu>
2020-11-19 23:28:04 -0800

    frontend for requests

--- covid19-server/src/models/Transaction.js
+++ covid19-server/src/models/Transaction.js
@@ -2,24 +2,23 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const t = require("./Transaction");
-
 const options = {collection: "accounts"} // idk what this is 
 //how do you add empty spot to befilled out/ 
-let companyAccount = new Schema({
-        companyName: {
+let Transaction = new Schema({
+        from: {
             type: String
         },
-        companyAddress: {
+        to: {
             type: String
         },
-        supplyChainRole: {
+        date: {
+            type: Number
+        },
+        TokenID: {
             type: Number
         },
         //referencest to the list of Transacction objects
-        transactionHistory: {
-            type: []
-        }
+
     },
     options
 )
