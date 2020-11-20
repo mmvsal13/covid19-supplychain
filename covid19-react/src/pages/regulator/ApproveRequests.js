d11a00f23d71482639c5561c00fb7fe9fc9cea2f
Kentaro Vadney <kentarovadney@berkeley.edu>
2020-11-19 21:59:26 -0800

    ApproveRequests + dummy data

--- covid19-react/src/pages/regulator/ApproveRequests.js
+++ covid19-react/src/pages/regulator/ApproveRequests.js
@@ -1,11 +1,23 @@
 import React from 'react';
 // import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
+import "./DummyApproveRequests.js"
+
+import { Layout, Header, Footer, Sider, Content } from antd;
+
 function Approve_requests() {
     return (
-        <div style={{ fontSize: 30, fontWeight: 'bold' }}>
-            regulator will be looking to approve requests here
-        </div>
+        <Layout>
+            <Header>TOKEN REQUESTS</Header>
+            <Layout>
+                <Sider>left sidebar</Sider>
+                <Content>
+                    ReactDOM.render(<Table columns={columns} dataSource={data} />, mountNode);
+                </Content>
+                <Sider>right sidebar</Sider>
+            </Layout>
+            <Footer>footer</Footer>
+        </Layout>
     );
 }
 
