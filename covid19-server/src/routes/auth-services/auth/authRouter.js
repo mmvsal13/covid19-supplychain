80c32593d0d17ab3d6edb89db1e691964faeabb7
emmaguo13 <emmaguo@berkeley.edu>
2020-12-03 09:46:16 -0800

    metamask auth

--- covid19-server/src/routes/auth-services/auth/authRouter.js
+++ covid19-server/src/routes/auth-services/auth/authRouter.js
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
     return res.status(200).send("Covid19 Supplychain Auth Router")
 })
 
-//
+//checking in backend if there is an account 
+//router.route('/').post(controller.create)
 
 module.exports = router;
