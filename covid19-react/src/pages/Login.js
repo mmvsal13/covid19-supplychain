135a75d05b96e3e21b38e0b6b6924469e288ddb5
Tim <timg51237@gmail.com>
2020-12-04 12:22:32 -0800

    fixed image bug

--- covid19-react/src/pages/Login.js
+++ covid19-react/src/pages/Login.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { Button, Link } from 'antd';
 import Web3 from 'web3';
 import axios from 'axios';
-import Center from '../components/center.js'
-
+import Center from '../components/center.js';
+import Image from './image-1.png';
 //import { Auth } from '../types';
 
 let web3 = undefined; // Will hold the web3 instance
@@ -15,26 +15,24 @@ function Login(props) {
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState('');
 
-    function handleAuthenticate(
-        publicAddress,
-        signature,
-     ) {
-        fetch(`${process.env.REACT_APP_BACKEND_URL}/auth`, { //change
+    function handleAuthenticate(publicAddress, signature) {
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/auth`, {
+            //change
             body: JSON.stringify({ publicAddress, signature }),
             headers: {
-              'Content-Type': 'application/json',
+                'Content-Type': 'application/json',
             },
             method: 'POST',
-          }).then((response) => response.json());
-     }
-    
+        }).then((response) => response.json());
+    }
+
     async function handleLogin() {
         const { onLoggedIn } = props;
-        console.log("handling login")
+        console.log('handling login');
         // Check if MetaMask is installed
         if (!window.ethereum) {
             // what is window
-            console.log("checking for metamask")
+            console.log('checking for metamask');
             window.alert('Please install MetaMask first.');
             return;
         }
@@ -59,17 +57,14 @@ function Login(props) {
         }
 
         const publicAddress = coinbase.toLowerCase();
-        console.log(publicAddress)
+        console.log(publicAddress);
         setLoading(true);
 
         // Look if user with current publicAddress is already present on backend
 
-        fetch(
-        `http://localhost:4000/api/users?publicAddress=${publicAddress}`,{
-
-            headers: {'Content-Type': 'application/json'},
-            })
-
+        fetch(`http://localhost:4000/api/users?publicAddress=${publicAddress}`, {
+            headers: { 'Content-Type': 'application/json' },
+        })
             .then((response) => response)
             // If yes, retrieve it. If no, create it.
             .then((text) => console.log(text))
@@ -84,90 +79,103 @@ function Login(props) {
                 window.alert(err);
                 setLoading(false);
             });
-            
-     }
-
-     async function initialValidation() {
-         return name.length > 0 && address.length > 0 && password.length > 0;
     }
 
-    async function handleSignMessage (
-        publicAddress,
-        nonce,
-      ) {
+    async function initialValidation() {
+        return name.length > 0 && address.length > 0 && password.length > 0;
+    }
+
+    async function handleSignMessage(publicAddress, nonce) {
         try {
-          const signature = await web3.eth.personal.sign(
-            `I am signing my one-time nonce: ${nonce}`,
-            publicAddress,
-            '' // MetaMask will ignore the password argument here
-          );
-        } catch(err){
-            console.log(err)
+            const signature = await web3.eth.personal.sign(
+                `I am signing my one-time nonce: ${nonce}`,
+                publicAddress,
+                '' // MetaMask will ignore the password argument here
+            );
+        } catch (err) {
+            console.log(err);
         }
     }
 
-    function handleSignup ()  {
-        window.open("/register");
-      };
+    function handleSignup() {
+        window.open('/register');
+    }
 
-    
     //maybe also add helpful links to get people to make metamask acct
     return (
+        <div
+            style={{
+                width: '100vw',
+                height: '100vh',
+                backgroundColor: '#E9FFFA',
 
-<div style={{ width: '100vw', height: '100vh', backgroundColor: '#E9FFFA',
-                
                 position: 'absolute',
-        
-        }}>
-            
+            }}
+        >
             <Center />
-            
-            <div style={{ fontSize: 30, fontWeight: 'bold',
-            position: 'absolute',
-            zIndex: '0',
-            margin: 'auto',
-            top: '50%',
-        left: '50%',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)' 
-        
-        }}>
-                    
-            <div className="LogIn" style = {{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-around',
-                    flexDirection: 'column'
-                }}>
-                   <img src = '/image-1.png' />
-                <h3 style = {{
-                        fontSize: '40px',
-                        color: '#620E0E',
-                        fontWeight: '800',
-                        lineHeight: '60px'
-                    }}>Sign in with Metamask</h3>
-                <Button variant="primary" type="submit" onClick={handleLogin} style = {{
-                        color: 'white',
-                        backgroundColor: '#FB8027',
-                        width: '250px',
-                        height: '65px',
-                        fontSize: '25px' }}>
-                    Continue
-                </Button>
-                <a  style = {{ 
-                    color: '#C71F03',
-                    fontWeight: '600',
-                    fontSize: '20px',
-                    style: 'normal'
+
+            <div
+                style={{
+                    fontSize: 30,
+                    fontWeight: 'bold',
+                    position: 'absolute',
+                    zIndex: '0',
+                    margin: 'auto',
+                    top: '50%',
+                    left: '50%',
+                    marginRight: '-50%',
+                    transform: 'translate(-50%, -50%)',
                 }}
-                 className="MakeAccount" href="/register">
-                    I don't have an account
-                </a>
-            </div>
-            
+            >
+                <div
+                    className="LogIn"
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-around',
+                        flexDirection: 'column',
+                    }}
+                >
+                    <img src={Image} />
+                    <h3
+                        style={{
+                            fontSize: '40px',
+                            color: '#620E0E',
+                            fontWeight: '800',
+                            lineHeight: '60px',
+                        }}
+                    >
+                        Sign in with Metamask
+                    </h3>
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        onClick={handleLogin}
+                        style={{
+                            color: 'white',
+                            backgroundColor: '#FB8027',
+                            width: '250px',
+                            height: '65px',
+                            fontSize: '25px',
+                        }}
+                    >
+                        Continue
+                    </Button>
+                    <a
+                        style={{
+                            color: '#C71F03',
+                            fontWeight: '600',
+                            fontSize: '20px',
+                            style: 'normal',
+                        }}
+                        className="MakeAccount"
+                        href="/register"
+                    >
+                        I don't have an account
+                    </a>
+                </div>
             </div>
         </div>
-                        
     );
 }
 
--- covid19-react/src/pages/Status.js
+++ covid19-react/src/pages/Status.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import LoadingScreen from './LoadingScreen.js';
 import { Link } from 'react-router-dom';
-import Navbar from '../components/navbar.js';
+import Navbar from '../components/Navbar.js';
 import UserHeader from '../components/UserHeader.js';
 import { Button, Tabs, Input, Upload, message } from 'antd';
 import { Table, Tag, Space } from 'antd';
-
-
 
 // import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
@@ -34,25 +32,25 @@ function Status() {
             title: 'Approved Status',
             key: 'tags',
             dataIndex: 'tags',
-                 render: tags => (
-                     <>
-                         {tags.map((tag) => {
-                             let color = 'magenta';
-                             if (tag === 'APPROVED') {
-                                 color = 'green';
-                             } else if (tag === 'PENDING') {
-                                 color = 'yellow';
-                             } else if (tag === 'REJECTED') {
-                                 color = 'red';
-                             }
-                             return (
-                                 <Tag color={color} key={tag}>
-                                     {tag.toUpperCase()}
-                                 </Tag>
-                             );
-                         })}
-                     </>
-                 ),
+            render: (tags) => (
+                <>
+                    {tags.map((tag) => {
+                        let color = 'magenta';
+                        if (tag === 'APPROVED') {
+                            color = 'green';
+                        } else if (tag === 'PENDING') {
+                            color = 'yellow';
+                        } else if (tag === 'REJECTED') {
+                            color = 'red';
+                        }
+                        return (
+                            <Tag color={color} key={tag}>
+                                {tag.toUpperCase()}
+                            </Tag>
+                        );
+                    })}
+                </>
+            ),
         },
     ];
 
@@ -65,7 +63,7 @@ function Status() {
             address: 'mmvsal13@berkeley.edu',
             tags: ['REJECTED'],
         },
-    ]
+    ];
 
     const getData = async () => {
         console.log('clicked');
@@ -78,26 +76,26 @@ function Status() {
 
     return (
         <>
-        <div style={{ width: '100vw', height: '100vh', backgroundColor: '#E9FFFA' }}>
-            <Navbar/>
-            <div style={{ margin: '5vw' }}>
-                <UserHeader active="Status" />
-                <div
-                    style={{
-                        width: '90vw',
-                        height: '70vh',
-                        backgroundColor: 'rgba(255, 240, 219, 1)',
-                        borderBottomLeftRadius: '2vw',
-                        borderBottomRightRadius: '2vw',
-                        padding: '3vh 5vw 3vh 5vw',
-                    }}  
-                >
-                    <Table columns={columns} dataSource={data} size="middle"/>
+            <div style={{ width: '100vw', height: '100vh', backgroundColor: '#E9FFFA' }}>
+                <Navbar />
+                <div style={{ margin: '5vw' }}>
+                    <UserHeader active="Status" />
+                    <div
+                        style={{
+                            width: '90vw',
+                            height: '70vh',
+                            backgroundColor: 'rgba(255, 240, 219, 1)',
+                            borderBottomLeftRadius: '2vw',
+                            borderBottomRightRadius: '2vw',
+                            padding: '3vh 5vw 3vh 5vw',
+                        }}
+                    >
+                        <Table columns={columns} dataSource={data} size="middle" />
+                    </div>
                 </div>
             </div>
-        </div>
         </>
-        );  
+    );
 }
 
 export default Status;
